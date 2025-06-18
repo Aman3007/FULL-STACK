6cf3c6efc1d1d24e6312cdfc3150a4434cde950f
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-18 02:05:58 -0500

    Update Navbar.jsx

--- client/src/componenrts/Navbar.jsx
+++ client/src/componenrts/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar=()=>{
         <nav id="Navbar">
             <div className="navrow">
               <NavLink  className={({ isActive }) => (isActive ? "active-link" : "Nav")} to="/">Home</NavLink>
-               <NavLink  className={({ isActive }) => (isActive ? "active-link" : "Nav")} to="/about">About</NavLink>
+               {/* <NavLink  className={({ isActive }) => (isActive ? "active-link" : "Nav")} to="/about">About</NavLink> */}
                 <NavLink  className={({ isActive }) => (isActive ? "active-link" : "Nav")} to="/register">Register</NavLink>
                 <NavLink  className={({ isActive }) => (isActive ? "active-link" : "Nav")} to="/login">Login</NavLink>
             </div>          
