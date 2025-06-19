c760d86789be6f5d56c11de453a86e95a34b6355
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-19 08:44:16 -0500

    Update auth-niddelware.js

--- server/middleware.js/auth-niddelware.js
+++ server/middleware.js/auth-niddelware.js
@@ -2,18 +2,17 @@ import { VerifyJWTToken } from "../Model/User.js";
 export const verifyAuthenticationToken=((req,res,next)=>{
     const token =req.cookies.access_token;
     if(!token){
-        req.User=null;
-  
-        return next()
+           return res.status(401).json({ message: "No token, access denied" });
     }
 
     try {
         const decodeToken = VerifyJWTToken(token);
         req.User=decodeToken;
         console.log(req.User)
+         return next()
 
     } catch (error) {
-        console.log(error)
+       return res.status(403).json({ message: "Invalid token" });
     }
-            return next()
+           
 })
