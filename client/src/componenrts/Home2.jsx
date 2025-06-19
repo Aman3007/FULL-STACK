c91ebcd36eff95cc8ec47611167b532dbb604587
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-19 08:38:25 -0500

    Update Home2.jsx

--- client/src/componenrts/Home2.jsx
+++ client/src/componenrts/Home2.jsx
@@ -1,14 +1,54 @@
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios"
 function Home2() {
+  const [authorized, setAuthorized] = useState(false);
 const { user, token } = useLocation().state|| {};
-
-  if (!user) return <p>No user info available.</p>;
 const navigate = useNavigate();
 
-  const handleClick = () => {
+useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await axios.get("https://full-stack-j0as.onrender.com/api/users/myhome", {
+          withCredentials: true,
+        });
+
+        if (res.status === 200) {
+          setAuthorized(true);
+      
+        }
+      } catch (err) {
+        alert("You are not logged in!");
+        navigate("/");
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+
+  const handlesubmit = async () => {
+     try {
+      const res = await axios.post("https://full-stack-j0as.onrender.com/api/users/logout", {}, {
+        withCredentials: true,
+      });
+
+      if (res.status === 200 && res.data.message === "Logged out successfully") {
+        navigate("/");
+      }
+    } catch (err) {
+      console.error("Logout failed:", err);
+      alert("Failed to logout. Try again.");
+    }
+  }
+
+     const handleClick = () => {
     navigate("/secrets",{ state: { user, token } });  
   };
+
+  if (!authorized) return null;
+
   return (
     <>
 <div className="userinfo">
@@ -24,11 +64,12 @@ const navigate = useNavigate();
     <a onClick={handleClick}>Add Your's</a>
   </div>
 </div>
+
+  <button className="logout" onClick={handlesubmit} >
+Log-out
+  </button>
+
 </div>
-
-
-
-
     </>
    
   );
